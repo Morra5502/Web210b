ca6cc8d51440c5934cfd1366f77c91f9326568dd
Zhirzhik <157021598+Zhirzhik@users.noreply.github.com>
2024-01-22 00:48:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // JavaScript source code
+function addData() {
+    // Get input values 
+    var name =
+        document.getElementById("nameInput").value;
+    var quantity =
+        document.getElementById("quantityInput").value;
+
+    // Get the table and insert a new row at the end 
+    var table = document.getElementById("outputTable");
+    var newRow = table.insertRow(table.rows.length);
+
+    // Insert data into cells of the new row 
+    newRow.insertCell(0).innerHTML = name;
+    newRow.insertCell(1).innerHTML = quantity;
+
+    // Clear input fields 
+    clearInputs();
+}
+function clearInputs() {
+
+    // Clear input fields 
+    document.getElementById("nameInput").value = "";
+    document.getElementById("quantityInput").value = "";
+}
